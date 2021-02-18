4ea100f480e5fae79feecf4088e43c60c0c49188
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-02-17 23:42:54 -0800

    Revized Resizablebag
    added code and revised for resize

--- BagProject/ResizableArrayBag.java
+++ BagProject/ResizableArrayBag.java
@@ -4,13 +4,39 @@ import java.util.ArrayList;
 public class ResizableArrayBag<T> implements BagInterface<T> {
 private final T[] bag;
 private static final int numberOfEntries;
-    public ResizableArrayBag()
+    public ResizableArrayBag(int desiredCapacity)
     {
-        numberOfEntries =0;
-        @SuppressWarnings("unchecked")
-        T[] tempBag=(T[]new Object[capacity]);
-        bag = tempBag;
+        if(desiredCapacity<=MAX_CAPACITY)
+        {
+            numberOfEntries =0;
+            @SuppressWarnings("unchecked")
+            T[] tempBag=(T[]new Object[capacity]);
+            bag = tempBag;
+            integrityOK=true;
+        }
+        else
+            throw new IllegalStateException("Attempt to create a bag whose"+"capacity exceeds allowed maximum.");
     }
+
+    private void checkCapacity(int capacity)
+    {
+        if(capacity>MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose"+"capacity exceeds allowed"+"maximum of"+MAX_CAPACITY)
+    }
+
+    private void doubleCapacity()
+    {
+        int newLength=2*bag.length;
+        checkCapacity(newLength);
+        bag=Arrays.copyOf(bag, newLength);
+    }
+
+    private void checkIntegrity()
+    {
+        if (!integrityOK)
+            throw new SecurityException("ResizableArray object is corrupt.");
+    }
+
     public String union() {
         return null;
         // TODO Auto-generated method stub
@@ -29,59 +55,110 @@ private static final int numberOfEntries;
     }
 
 
-    public int getCurrentSize() {
-        // TODO Auto-generated method stub
-        return 0;
+    public int getCurrentSize() 
+    {
+        return numberOfEntries;
     }
 
 
     public boolean isEmpty() 
     {
-        return numberOfEntries == bag.length;
+        return numberOfEntries == 0;
     }
 
 
-    public boolean add(Object newEntry) {
+    public boolean add(T newEntry) 
+    {
+        checkIntegrity();
         boolean result = true;
-        if (isArrayFull()) {
-               result = false;
+        if (isArrayFull()) 
+        {
+               doubleCapacity;
         }
-        else{  // Assertion: result is true here
-         bag[numberOfEntries] = newEntry;
+
+         // Assertion: result is true here
+        bag[numberOfEntries] = newEntry;
         numberOfEntries++;
-        } // end if
         
-        return result;
+        
+        return true;
   
     }
 
-
-    public Object remove() {
-        // TODO Auto-generated method stub
-        return null;
+    public T remove()
+    {
+        checkIntegrity();
+        T result=removeEntry(numberOfEntries-1);
+        return result;
     }
 
-    public boolean remove(Object anEntry) {
-        // TODO Auto-generated method stub
-        return false;
+    private T remove(int givenIndex) 
+    {
+        T result=null;
+
+        if(!isEmpty()&&(givenIndex>=0))
+        {
+            result=bag[givenIndex];
+            bag[givenIndex]=bag[numberOfEntries-1];
+            bag[numberOfEntries-1]=null;
+            numberOfEntries--;
+        }
+        return result;
+    }
+
+    public boolean remove(T anEntry) 
+    {
+        checkIntegrity();
+        int index = getIndexOf(anEntry);
+        T result = removeEntry(index);
+        return anEntry.equals(result);
+    }
+
+    private int getIndexOf(T anEntry)
+    {
+        int where =-1;
+        boolean found = false;
+        int index = 0;
+
+        while (!found &&(index<numberOfEntries))
+        {
+            if (anEntry.equals(bag[index]))
+            {
+                found=true;
+                where=index;
+            }
+            index++;
+        }
+        return where;
+    }
+
+    public void clear() 
+    {
+        while(!isEmpty())
+            remove();
+
     }
 
 
-    public void clear() {
-        // TODO Auto-generated method stub
+    public int getFrequencyOf(T anEntry) 
+    {
+        checkIntegrity();
+        int counter =0;
 
+        for (int index =0; index < numberOfEntries; index++)
+        {
+            if(anEntry.equals(bag[index]))
+            {
+                counter++;
+            }
+        }
     }
 
 
-    public int getFrequencyOf(Object anEntry) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    public boolean contains(Object anEntry) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean contains(T anEntry) 
+    {
+        checkIntegrity();
+        return getIndexOf(anEntry)>-1;
     }
 
 
