3947494eb87768f6dfb625370eeb273a88ddc354
Samuel <chihsamuel@gmail.com>
2021-02-22 13:58:56 -0800

    testing

---
+++ BagProject/ArrayBagTest.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+public class ArrayBagTest 
+{
+
+	public static void main(String[] args) 
+	{
+		BagInterface<String> b1 = new ResizableArrayBag<String>();
+		BagInterface<String> b2 = new ResizableArrayBag<String>();
+
+		b1.add("Hello");//a
+		b1.add("Hello");//a
+		b1.add("Hello");//a
+		b1.add("how are");//b
+		b1.add("you?");//c
+		b1.add("ハワユ");//e
+
+		b2.add("how are");//b
+		b2.add("how are");//b
+		b2.add("/");//d
+		b2.add("/");
+		b2.add("ハワユ");//e
+		//difference should only "Hello" "you?" "ハワユ"
+
+		// b2.add("Hello");
+		// b2.add("/");
+		// b2.add("ハワユ");
+
+		System.out.println("bag1: "+Arrays.toString(b1.toArray()));
+		System.out.println("bag2: "+Arrays.toString(b2.toArray()));
+
+		//System.out.println("bag1 union bag2: "+ Arrays.toString(b1.union(b2).toArray()));
+		//System.out.println("bag1 intersection bag2: "+ Arrays.toString(b1.intersection(b2).toArray()));
+		System.out.println("bag1 difference bag2: "+ Arrays.toString(b1.difference(b2).toArray()));
+	}
+
+}
--- BagProject/ResizableArrayBag.java
+++ BagProject/ResizableArrayBag.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package BagProject;
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -37,6 +38,50 @@ private static final int numberOfEntries;
     {
         if(capacity>MAX_CAPACITY)
             throw new IllegalStateException("Attempt to create a bag whose"+"capacity exceeds allowed"+"maximum of"+MAX_CAPACITY)
+=======
+/**A class of bags whose entries are stored in a resizable array
+ * @author Miranda Silva, Samuel Chih
+ */
+import java.util.Arrays;
+
+public class ResizableArrayBag<T> implements BagInterface<T> {
+    private Node firstNode;
+    private T[] bag;
+    private static final int MAX_CAPACITY = 1000;
+    private boolean integrityOK;
+    private int numberOfEntries=0;
+
+    public ResizableArrayBag(int desiredCapacity)
+    {
+        firstNode = null;
+        integrityOK=false;
+        if(desiredCapacity<=MAX_CAPACITY)
+        {
+            
+            @SuppressWarnings("unchecked")
+            T[] tempBag = (T[])new Object[desiredCapacity]; 
+            bag = tempBag;
+            integrityOK = true;
+        }
+        else
+            throw new IllegalStateException("Attempt to create a bag whose"+"capacity exceeds allowed maximum.");
+    } 
+    public ResizableArrayBag(){
+        firstNode = null;
+        integrityOK=false;
+            @SuppressWarnings("unchecked")
+            T[] tempBag = (T[])new Object[MAX_CAPACITY]; 
+            bag = tempBag;
+            integrityOK = true;
+        
+    }
+
+
+    private void checkCapacity(int capacity)
+    {
+        if(capacity>MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose"+"capacity exceeds allowed"+"maximum of"+MAX_CAPACITY);
+>>>>>>> Stashed changes
     }
 
     private void doubleCapacity()
@@ -52,6 +97,7 @@ private static final int numberOfEntries;
             throw new SecurityException("ResizableArray object is corrupt.");
     }
 
+<<<<<<< Updated upstream
     public String union() {
         return null;
         // TODO Auto-generated method stub
@@ -69,18 +115,29 @@ private static final int numberOfEntries;
 
     }
 
+=======
+>>>>>>> Stashed changes
 
     public int getCurrentSize() 
     {
         return numberOfEntries;
     }
 
+<<<<<<< Updated upstream
+=======
+    public boolean isArrayFull()
+    {
+        return numberOfEntries==bag.length;
+    }
+
+>>>>>>> Stashed changes
 
     public boolean isEmpty() 
     {
         return numberOfEntries == 0;
     }
 
+<<<<<<< Updated upstream
 
     public boolean add(T newEntry) 
     {
@@ -98,16 +155,41 @@ private static final int numberOfEntries;
         
         return true;
   
+=======
+    public boolean add(T newEntry) 
+    {
+        if (integrityOK){
+        boolean result = true;
+        if (isArrayFull()){
+        doubleCapacity();
+        }
+        else{ 
+        bag[numberOfEntries] = newEntry;
+        numberOfEntries++;
+        } 
+        return result;
+        }
+        else
+            throw new SecurityException("ArrayBag object is corrupt.");
+>>>>>>> Stashed changes
     }
 
     public T remove()
     {
+<<<<<<< Updated upstream
         checkIntegrity();
         T result=removeEntry(numberOfEntries-1);
         return result;
     }
 
     private T remove(int givenIndex) 
+=======
+        T result = removeEntry(numberOfEntries - 1);
+        return result;
+    }
+
+    private T removeEntry(int givenIndex) 
+>>>>>>> Stashed changes
     {
         T result=null;
 
@@ -157,6 +239,7 @@ private static final int numberOfEntries;
 
     public int getFrequencyOf(T anEntry) 
     {
+<<<<<<< Updated upstream
         checkIntegrity();
         int counter =0;
 
@@ -167,11 +250,28 @@ private static final int numberOfEntries;
                 counter++;
             }
         }
+=======
+        int frequency = 0;
+
+		int counter=0;
+		Node currentNode=firstNode;
+		while ((counter<numberOfEntries)&&(currentNode !=null))
+		{
+			if(anEntry.equals(currentNode !=null))
+			{
+				frequency++;
+			}
+			counter++;
+			currentNode = currentNode.next;
+		}
+		return frequency;
+>>>>>>> Stashed changes
     }
 
 
     public boolean contains(T anEntry) 
     {
+<<<<<<< Updated upstream
         checkIntegrity();
         return getIndexOf(anEntry)>-1;
     }
@@ -184,10 +284,166 @@ private static final int numberOfEntries;
         for (int index =0; index < numberOfEntries; index++)
         {
             result[index] = bag[index];
+=======
+        boolean found = false;
+		Node currentNode=firstNode;
+
+		while (!found && (currentNode !=null))
+		{
+			if (anEntry.equals(currentNode.data))
+				found = true;
+				else
+					currentNode =currentNode.next;
+		}
+		return found;
+    }
+
+    public T[] toArray() {
+      T[] copy = (T[])new Object[numberOfEntries];
+        for (int i = 0; i < this.numberOfEntries; i++) {
+            if(bag[i]!=null){
+                copy[i] = bag[i];
+            } else{
+                i++;
+            }
+        }
+        return copy;
+    }
+
+/*     public T[] toArray()
+    {
+        @SuppressWarnings("unchecked")
+		T[] result=(T[])new Object[numberOfEntries];
+		int index=0;
+		Node currentNode=firstNode;
+		while((index<numberOfEntries)&&(currentNode !=null))
+		{
+			result[index] =currentNode.data;
+			index++;
+			currentNode=currentNode.next;
+		}
+		return result;
+    } */
+
+    private class Node {
+		private T data;
+		private Node next;
+
+		private Node(T dataPortion) {
+			this(dataPortion, null);
+		}
+
+		private Node(T dataPortion, Node nextNode) {
+			data = dataPortion;
+			next = nextNode;
+		}
+	}
+
+    @Override
+    public BagInterface<T> union(BagInterface<T> anotherBag) 
+    {
+        BagInterface < T > result = new ResizableArrayBag<>();
+        T[] mine = this.toArray();
+        for (T elem : mine) 
+        {
+            result.add(elem);
+        }
+        T[] others = anotherBag.toArray();
+        for (T elem : others) 
+        {
+            result.add(elem);
+>>>>>>> Stashed changes
         }
         return result;
     }
 
+<<<<<<< Updated upstream
 
 
+=======
+    @Override
+    public BagInterface<T> intersection(BagInterface<T> anotherBag) 
+    {
+        BagInterface < T > result = new ResizableArrayBag<>();
+        BagInterface < T > finalResult = new ResizableArrayBag<>();
+        T[] mine = this.toArray();
+        for (T elem : mine) 
+        {
+            result.add(elem);
+        }
+        T[] others = anotherBag.toArray();
+        for (T elem : others) 
+        {
+            if(result.contains(elem))
+        {
+                finalResult.add(elem);
+        }
+        }
+        return finalResult;
+    }
+    
+    public BagInterface<T> difference(BagInterface<T> anotherBag) {
+        BagInterface <T> result = new ResizableArrayBag<>();
+        T[] mine = this.toArray();
+        T[] others = anotherBag.toArray();
+        a:
+        for (int i=0; i<mine.length;i++) {
+            boolean temp =true;
+            for (int k=0; k<others.length;k++){
+                if(mine[i]==others[k]){
+                    temp=false;
+                }
+                if (temp==true){
+                for (int j=mine.length-1; j>=0;j--){ 
+                 if((k==others.length-1)&&mine[i]!=mine[j]){
+                    result.add(mine[i]);
+                    break a;
+                 }
+                }
+            }
+            }
+        }
+        /*  a:
+        for (T i : mine) {
+            b:
+            for (T k : others) {
+                if(i!=k){
+                    System.out.println("I: "+i+", K out: "+k);
+                    System.out.println("mine: "+mine+", other out: "+others);
+                    result.add(i);
+                    break a;
+                }
+                else{
+                    break ;
+                }  
+                  
+             }
+        } */
+/*         for (T elem : mine) {
+            result.add(elem);
+        } */
+        return result;
+    }
+
+/*     @Override
+    public BagInterface<T> difference(BagInterface<T> anotherBag) 
+    {
+        BagInterface < T > result = new ResizableArrayBag<>();
+        T[] mine = this.toArray();
+        for (T elem : mine) 
+        {
+        result.add(elem);
+        }
+        T[] others = anotherBag.toArray();
+        for (T elem : others) 
+        {
+        if(result.contains(elem))
+        {
+        result.remove(elem);
+        }
+        }
+        return result;
+        }  */
+       
+>>>>>>> Stashed changes
 }
