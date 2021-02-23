41121b414d33a47cf11f882cee036e5577c69023
Samuel <chihsamuel@gmail.com>
2021-02-22 19:37:49 -0800

    Update LinkedBagTest.java

--- BagProject/LinkedBagTest.java
+++ BagProject/LinkedBagTest.java
@@ -13,25 +13,24 @@ public class LinkedBagTest {
 	b2.add("how are");//b
 	b2.add("how are");//b
 	b2.add("/");//d
-	b2.add("sdadsfde");//e
+	b2.add("ハワユ");//e
 	}
 
 	@Test
 	public void testUnion(){
-		System.out.println("[Hello, how are, you?, how are, how are, /, sdadsfde]");
-		assertEquals("[Hello, how are, you?, how are, how are, /, sdadsfde]",Arrays.toString(b1.union(b2).toArray()));
+		System.out.println("[Hello, how are, you?, how are, how are, /, ハワユ]");
+		assertEquals("[Hello, how are, you?, how are, how are, /, ハワユ]",Arrays.toString(b1.union(b2).toArray()));
 	}
 
 	@Test
 	public void testDifference(){
 		System.out.println("testDifference: [Hello, you?]");
 		assertEquals("[Hello, you?]",Arrays.toString(b1.difference(b2).toArray()));
-		System.out.println("[how are, /, sdadsfde]");
-		assertEquals("[how are, /, sdadsfde]",Arrays.toString(b2.difference(b1).toArray()));
+		System.out.println("[how are, /, ハワユ]");
+		assertEquals("[how are, /, ハワユ]",Arrays.toString(b2.difference(b1).toArray()));
 	}
 	@Test
 	public void testIntersection(){
-
 		System.out.println("[how are]");
 		assertEquals("[how are]",Arrays.toString(b1.intersection(b2).toArray()));
 		System.out.println("[how are, how are]");
